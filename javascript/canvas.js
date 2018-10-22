c1f6ea4bb971f347ef52a56fab07487bbf756579
Paul <Pkim3928@gmail.com>
2018-10-22 17:51:02 -0400

    Update javascript/canvas.js

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -5,6 +5,7 @@ canvas.height = window.innerHeight;
 
 /*stands for context*/ 
 var c = canvas.getContext("2d");
+var ctx = canvas.getContext("2d");
 
 c.beginPath();
 c.moveTo(350, 200);
@@ -25,6 +26,7 @@ c.strokeStyle="white";
 c.lineWidth=5;
 c.stroke();
 
+
 c.beginPath();
 c.moveTo(900, 200);
 c.lineTo(1250, 200);
@@ -40,5 +42,13 @@ c.lineTo(1400, 200);
 c.strokeStyle="white";
 c.lineWidth=5;
 c.stroke();
+
+
+ctx.font = "35px Helvetica";
+ctx.fillStyle = "white";
+ctx.textAlign = "center";
+ctx.fillText("About Me", 550,450); 
+ctx.fillText("Most Recent Jobs", 900,450); 
+ctx.fillText("Top Three Skills", 1250,450); 
 
 
